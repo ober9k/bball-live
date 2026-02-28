cd675a9fc1fbf64b12aace3f1e399a6ef962cc76
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-28 13:13:31 +1100

    Added work in progress for points, rebounds, assists

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,84 @@
+import { BoxScoreTable } from '@/components/box-score-table/box-score-table';
+import { mockPlayers } from '@/data/mock/players';
+import { PlayerStatsLog } from '@/types/logs/PlayerStatsLog';
+import { Player } from '@/types/Player';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BoxScoreTable],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('bball-live');
+
+  playerStatsLogs = signal(mockPlayers
+    .map((player) => App.generatePlayerStatsLog(player))
+  );
+
+  /**
+   * Duplicated logic for points/rebounds/assists (for now).
+   * @param playerStatsLog
+   * @param value
+   */
+  addPoints(playerStatsLog: PlayerStatsLog, value: number): void {
+    this.playerStatsLogs.update((playerStagsLogs) => {
+      const log = playerStagsLogs.find((log) => log.id === playerStatsLog.id);
+
+      if (log) {
+        log.stats.points = log.stats.points + value;
+      }
+
+      return [ ... playerStagsLogs ];
+    });
+  }
+
+  /**
+   * Duplicated logic for points/rebounds/assists (for now).
+   * @param playerStatsLog
+   * @param value
+   */
+  addRebounds(playerStatsLog: PlayerStatsLog, value: number): void {
+    this.playerStatsLogs.update((playerStagsLogs) => {
+      const log = playerStagsLogs.find((log) => log.id === playerStatsLog.id);
+
+      if (log) {
+        log.stats.rebounds = log.stats.rebounds + value;
+      }
+
+      return [ ... playerStagsLogs ];
+    });
+  }
+
+  /**
+   * Duplicated logic for points/rebounds/assists (for now).
+   * @param playerStatsLog
+   * @param value
+   */
+  addAssists(playerStatsLog: PlayerStatsLog, value: number): void {
+    this.playerStatsLogs.update((playerStagsLogs) => {
+      const log = playerStagsLogs.find((log) => log.id === playerStatsLog.id);
+
+      if (log) {
+        log.stats.assists = log.stats.assists + value;
+      }
+
+      return [ ... playerStagsLogs ];
+    });
+  }
+
+  /* this can be in a util function later */
+  private static generatePlayerStatsLog(player: Player): PlayerStatsLog {
+    const stats = {
+      points: 0,
+      rebounds: 0,
+      assists: 0,
+    };
+
+    return { id: player.id, player, stats };
+  }
+
 }
