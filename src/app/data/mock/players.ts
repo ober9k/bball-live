4db83a00695155939e615b67f63c22c3bd5572f7
Dean Hardaker <ober9k@users.noreply.github.com>
2026-03-03 17:01:54 +1100

    Added extra mock players for bench roles

--- src/app/data/mock/players.ts
+++ src/app/data/mock/players.ts
@@ -9,4 +9,10 @@ export const mockPlayers: Array<Player> = [
   { id: id++, name: "L. James", number: "23", },
   { id: id++, name: "K. Love", number: "0", },
   { id: id++, name: "T. Thompson", number: "13", },
+  /* bench players for the same team */
+  { id: id++, name: "I. Shumpert", number: "4", },
+  { id: id++, name: "R. Jefferson", number: "24", },
+  { id: id++, name: "M. Dellavedova", number: "8", },
+  { id: id++, name: "C. Frye", number: "9", },
+  { id: id++, name: "M. Williams", number: "52", },
 ];
